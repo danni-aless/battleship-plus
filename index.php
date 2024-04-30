5c8c98ca631212209178f4fe693ec03dc247e47b
RanieroCompagnoni <compagnoni.1985039@studenti.uniroma1.it>
2024-04-30 17:11:12 +0200

    aggiunta funzione per evidenziare la pagina

--- index.php
+++ index.php
@@ -67,6 +67,15 @@
     });
 });
 </script>
+<script>
+    $(document).ready(function() {
+    $(".nav-link").click(function() {
+        $(".nav-link").removeClass("active");
+        $(this).addClass("active");
+    });
+});
+
+</script>
 
 </head>
 
