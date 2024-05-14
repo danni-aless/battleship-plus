4f0293aba6f3a6003407bc6845554f189da82e7b
danni-aless <alessandro.dannibale02@gmail.com>
2024-05-14 15:44:45 +0200

    corretto errore default.jpg

--- signup.php
+++ signup.php
@@ -13,13 +13,12 @@
             $query = "select * from login where email = '$email' or username='$username'";
             $result = mysqli_query($db_conn,$query);
             if (mysqli_num_rows($result) == 0) {
-                $query_insert = "insert into login (`username`,`email`, `password`,`data_iscrizione`) values ('$username', '$email', '$password',NOW())";
+                $query_insert = "insert into login (`username`,`email`,`password`,`data_iscrizione`,`image`) values ('$username','$email','$password',NOW(),'default.jpg')";
                 $result = mysqli_query($db_conn,$query_insert);
                 $_SESSION['logged_in'] = true;
                 $_SESSION['username'] =  $username;
                 $_SESSION['email'] =  $email;
                 $_SESSION['password'] =  $password;
-                $_SESSION['image'] =  $image;
             }else{
                 $_SESSION['signup_err_msg'] =  "Email o Username Non Disponibili!";
             }
