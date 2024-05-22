f94647f09e6bad9f982d71703d586a79e1e48b17
RanieroCompagnoni <compagnoni.1985039@studenti.uniroma1.it>
2024-05-22 12:14:14 +0200

    regole

--- rules.php
+++ rules.php
@@ -1,4 +1,15 @@
 <h1>Regole</h1>
 <div>
     <p>Questa è la pagina delle regole</p>
+    <div class="container">
+        <h1>Gioca a BattleShip Plus</h1>
+        <p>Crea una partita con le tue opzioni o Unisciti a una già in corso</p>
+        <p>Affonda tutte le navi del tuo avversario per vincere</p>
+        <p>Colpisci i power-up per ottenere poteri speciali</p>
+        <ul>
+            <li>power up arancione: colpisce tutta una riga</li>
+            <li>power up verde: colpisce tutta una colonna</li>
+            <li>power up blu: colpisce un'area 3x3</li>
+        </ul>
+    </div>
 </div>
